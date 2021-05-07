9ceaa264573f0e07ac257224c97cbb652fa9f87a
tuankiet116 <kietdat1612000@gmail.com>
2021-05-07 15:10:14 +0700

    fig module admin

--- admin/resource/security/security.php
+++ admin/resource/security/security.php
@@ -43,7 +43,7 @@ $fs_scriptpath = "../../resource/js/";
 $fs_denypath = "../../error.php";
 $wys_cssadd = array();
 $wys_cssadd = "/css/all.css";
-$fs_category = checkAccessCategory();
+//$fs_category = checkAccessCategory();
 //phan include file css
 
 //$load_header 			= '<link href="../../resource/css/css.css" rel="stylesheet" type="text/css">' . "\n";
