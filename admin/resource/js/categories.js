6cce785d0696ed5b20dcaf00d8a3ef891678de3c
tuankiet116 <kietdat1612000@gmail.com>
2021-06-01 18:11:25 +0700

    z

--- admin/resource/js/categories.js
+++ admin/resource/js/categories.js
@@ -1,4 +1,47 @@
 $(document).ready(function () {
+  //Select2 For Pick Website
+  $(".pick_website_select").select2({
+    ajax: { 
+      url: "../../../api/Controller/searchTerm.php",
+      type: "POST",
+      dataType: 'json',
+      delay: 250,
+      data: function (params) {
+        if(params.term == null){
+          var obj = {
+            "term": ""
+          } 
+        }else{
+          var obj = {
+          "term": params.term.trim()
+          } 
+        }
+        
+        return JSON.stringify(obj);
+      },
+      processResults: function (data, params) {
+        return {
+            results: $.map(data, function (item) {
+                return {
+                    text: item.web_name,
+                    id: item.web_id,
+                    image: checkdefault("data/web_icon/icon_default/default.png",item.web_icon),
+                    description: item.web_description,
+                    data: item
+                };
+            })
+        };
+      },
+      cache: false
+    },
+    placeholder: 'Search for a Website',
+    minimumInputLength: 0,
+    templateResult: formatRepoWebsite,
+    templateSelection: formatRepoSelectionWebsite
+  });
+
+
+
     $('.categories-parent-item').on('click', function(){
         $(this).siblings('.wapper-categories-child').slideToggle();
     });
@@ -15,13 +58,13 @@ $(document).ready(function () {
         $(id_image).siblings('svg').css('display', 'block');
     });
 
-    $(".input-image-container i").hover(function(e){
-        $(this).css("display", "block");
-    });
-    
-    $(".input-image-container i").mouseout(function(e){
-        $(this).css("display", "none");
-    });
+  $(".input-image-container i").hover(function(e){
+      $(this).css("display", "block");
+  });
+  
+  $(".input-image-container i").mouseout(function(e){
+      $(this).css("display", "none");
+  });
 
     // Input Image Processing When Image Is NULL
   $(".input-image").on("click", function(e){
@@ -43,7 +86,25 @@ $(document).ready(function () {
   $(".input-image img").mouseout(function(e){
     $(this).closest("div").siblings("i").css("display", "none");
   });
+
+  $('.categories-add').on('click', function(){
+    $('.categories-container-form').show();
+  });
+
+  $('.pick_website_select').on('change', function(){
+    var web_id = $('.pick_website_select').select2('data')[0].id;
+    $.ajax({
+      url: base_url + 'api/objects/categories.php',
+      method: 'POST',
+      data: {web_id: web_id},
+      success: function(data){
+        $('.categories-content').html(data);
+      }
+    });
+  })
 });
+
+var base_url = '../../../';
 
 //Make Information Image Get The FUCK Out Of Chrome Security And Change Data To Base64
 var exGetImg = function(extag, element) {
@@ -59,3 +120,48 @@ var exGetImg = function(extag, element) {
             $(element).siblings('img').attr('src', imgsSrc);    
     }
   }
+
+  //set default image which null value
+function checkdefault(default_value, check_parameter){
+  if(check_parameter == null){
+    return default_value;
+  }
+  return check_parameter;
+}
+
+  // Website function Select2
+  function formatRepoWebsite (repo) {
+    if (repo.loading) {
+      return repo.text;
+    }
+  
+    var $container = $(
+      "<div class='select2-result-website clearfix' id='result_website_"+repo.id+"'>" +
+        "<div class='select2-result-website__icon'><img src='" + base_url + repo.image + "' /></div>" +
+        "<div class='select2-result-website__meta'>" +
+          "<div class='select2-result-website__title'></div>" +
+          "<div class='select2-result-website__description'></div>" +
+        "</div>" +
+      "</div>"
+    );
+  
+    $container.find(".select2-result-website__title").text(repo.text);
+    $container.find(".select2-result-website__description").text(repo.description)
+  
+    return $container;
+  }
+  
+  function formatRepoSelectionWebsite (state) {
+    if (!state.id) {
+      return state.text;
+    }
+    var $state = $(
+      '<span id = "website_'+ state.id +'"><img class="img-flag" /> <span></span></span>'
+    );
+  
+    // Use .text() instead of HTML string concatenation to avoid script injection issues
+    $state.find("span").text(state.text);
+    $state.find("img").attr("src", base_url + state.image);
+  
+    return $state;
+  } //End Of Function Website Select2
