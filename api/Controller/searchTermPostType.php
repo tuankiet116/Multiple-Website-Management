aa46cc37e9b792c61f304b3d995a553cba454b33
doanphilong2k <doanphilong2k@gmail.com>
2021-06-04 10:11:03 +0700

    new .

--- api/Controller/searchTermPostType.php
+++ api/Controller/searchTermPostType.php
@@ -38,7 +38,7 @@ if($num>0){
             "post_type_description" => $row['post_type_description'],
             "post_type_show"        => $row['post_type_show'],
             "post_type_active"      => $row['post_type_active'],    
-            "cmp_id"                => $row['cmp_id'],
+            // "cmp_id"                => $row['cmp_id'],
             "post_type_term"        => $data->term
             
         );
--- home/Green_website/news.php
+++ home/Green_website/news.php
@@ -33,6 +33,12 @@ if (isset($_GET['name'])) {
     } else {
         $postNews = '';
     }
+
+    if (isset($_GET['postName'])) {
+        $post_name = $_GET['postName'];
+    } else {
+        $post_name = '';
+    }
 }
 
 $post_content = get_data_rows("SELECT * FROM post_detail");
@@ -219,7 +225,7 @@ $arr_contact = get_data_rows("SELECT * FROM configuration WHERE web_id = $web_id
                             if ($bread['cmp_rewrite_name'] == $breadcrumbs || $bread['cmp_id'] == $breadcrumbs) {
                                 $bread_parents = $bread['cmp_parent_id'];
                                 if ($bread_parents == null) {
-                                    echo '
+                                    echo'
                                         <a href="" target="_self">' . $bread['cmp_name'] . '</a>
 
                                         <span class="navigation-pipe">
@@ -228,7 +234,7 @@ $arr_contact = get_data_rows("SELECT * FROM configuration WHERE web_id = $web_id
                                 } else {
                                     $bread_name = get_data_rows("SELECT cmp_name FROM categories_multi_parent WHERE cmp_id = $bread_parents");
                                     foreach ($bread_name as $key => $b_name) {
-                                        echo '
+                                        echo'
                                         <a href="" target="_self">' . $b_name['cmp_name'] . '</a>
 
                                         <span class="navigation-pipe">
@@ -236,6 +242,16 @@ $arr_contact = get_data_rows("SELECT * FROM configuration WHERE web_id = $web_id
                                         </span>';
                                     }
                                 }
+                            }
+
+                            if ($post_name != '') {
+                                echo'
+                                    <a href="" target="_self">' . $post_name . '</a>
+
+                                    <span class="navigation-pipe">
+                                        <i class="fas fa-chevron-right"></i>
+                                    </span>
+                                ';
                             }
                         } ?>
 
@@ -267,37 +283,40 @@ $arr_contact = get_data_rows("SELECT * FROM configuration WHERE web_id = $web_id
                                                 $changeUrlBread = '&nameBreadcrumbs=' . $cate['cmp_id'];
                                             }
 
-                                            if ($g_post['cmp_id'] == $cate['cmp_id']) {
+                                            $get_pt_name = get_data_rows("SELECT * FROM post_type WHERE post_type_active = 1 AND web_id = $web_id AND post_type_id = $p_type_id AND allow_show_homepage = 0");
+                                            foreach ($get_pt_name as $key => $pt_name) {
+                                                if ($g_post['cmp_id'] == $cate['cmp_id']) {
 
-                                                $date = $g_post['post_datetime_create'];
-                                                $myDate = date("d-m-Y", strtotime($date));
+                                                    $date = $g_post['post_datetime_create'];
+                                                    $myDate = date("d-m-Y", strtotime($date));
 
-                                                echo '
-                                                <div class="container post-list">
-                                                    <div class="row">
-                                                        <div class="post-list-img col-lg-4 col-md-4 col-sm-6 col-6">
-                                                            <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '" target="_self">
-                                                                <img src="../../' . $g_post['post_image_background'] . '" alt="post list image">
-                                                            </a>
-                                                        </div>
+                                                    echo'
+                                                        <div class="container post-list">
+                                                            <div class="row">
+                                                                <div class="post-list-img col-lg-4 col-md-4 col-sm-6 col-6">
+                                                                    <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '&postName=' . $pt_name['post_type_title'] . '" target="_self">
+                                                                        <img src="../../' . $g_post['post_image_background'] . '" alt="post list image">
+                                                                    </a>
+                                                                </div>
 
-                                                        <div class="post-list-container col-lg-8 col-md-8 col-sm-6 col-6">                                                                                
-                                                            <div class="post-list-title">
-                                                                <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '" target="_self">
-                                                                    <p>' . $g_post['post_title'] . '</p>
-                                                                </a>
-                                                            </div>
-                                                            
-                                                            <div class="post-list-date">' . $myDate . '</div>
+                                                                <div class="post-list-container col-lg-8 col-md-8 col-sm-6 col-6">                                                                                
+                                                                    <div class="post-list-title">
+                                                                        <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '" target="_self">
+                                                                            <p>' . $g_post['post_title'] . '</p>
+                                                                        </a>
+                                                                    </div>
+                                                                    
+                                                                    <div class="post-list-date">' . $myDate . '</div>
 
-                                                            <div class="post-list-content">
-                                                                <p>' . $g_post['post_description'] . '</p>
-                                                            </div>
-                                                            
-                                                            <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '" class="post-list-button" target="_self">Xem tiếp</a>
-                                                        </div>
-                                                    </div>                 
-                                                </div>';
+                                                                    <div class="post-list-content">
+                                                                        <p>' . $g_post['post_description'] . '</p>
+                                                                    </div>
+                                                                    
+                                                                    <a href="news.php?' . $changeUrlName . '&title=' . $g_post['post_title'] . '&breadcrumbs=' . $cate['cmp_rewrite_name'] . $changeUrlBread . '&postNews=' . $g_post['ptd_id'] . '" class="post-list-button" target="_self">Xem tiếp</a>
+                                                                </div>
+                                                            </div>                 
+                                                        </div>';
+                                                }
                                             }
                                         }
                                     }
