5871ca6ab8ee386cb1f832eeabd40381f24a97cf
tuankiet116 <kietdat1612000@gmail.com>
2021-06-11 13:57:37 +0700

    fix query get all post type

--- api/objects/post_type.php
+++ api/objects/post_type.php
@@ -184,10 +184,12 @@
 
             $query = "SELECT post_type_id , post_type_title , post_type_description, 
                             post_type_show, web_name, post_type_active, 
-                            allow_show_homepage, web_id, GROUP_CONCAT(cmp_id) as cmp_list
+                            allow_show_homepage, web_id, GROUP_CONCAT(cmp_id) as cmp_list, GROUP_CONCAT(cmp_name) as cmp_name,
+                            GROUP_CONCAT(CAST(cmp_has_child AS int)) as cmp_has_child, GROUP_CONCAT(CAST(cmp_active AS int)) as cmp_active 
                         FROM (SELECT post_type.post_type_id , post_type.post_type_title , post_type.post_type_description, 
-                                post_type.post_type_show, website_config.web_name, post_type.post_type_active, 
-                                post_type.allow_show_homepage, post_type.web_id, cmp.cmp_id 
+                                    post_type.post_type_show, website_config.web_name, post_type.post_type_active, 
+                                    post_type.allow_show_homepage, post_type.web_id, cmp.cmp_id, cmp.cmp_has_child, 
+                                    cmp.cmp_active, cmp.cmp_name
                             FROM post_type 
                             INNER JOIN website_config ON website_config.web_id = post_type.web_id ".$query_website."
                             INNER JOIN categories_multi_parent cmp  ON  FIND_IN_SET(post_type.post_type_id, cmp.post_type_id) AND post_type.web_id = cmp.web_id 
