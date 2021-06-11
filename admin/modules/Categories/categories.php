b0df8e9d052032bb00f1035560ed4e0d9dea0a7d
DoThong0310 <dothong8211@gmail.com>
2021-06-11 11:29:24 +0700

    .

--- admin/modules/Categories/categories.php
+++ admin/modules/Categories/categories.php
@@ -6,6 +6,7 @@
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="<?php echo $name?>" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
