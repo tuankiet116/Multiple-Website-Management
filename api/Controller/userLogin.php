aba99793a40498875f8a7d8b00a7453a0c3a9847
tuankiet116 <kietdat1612000@gmail.com>
2021-07-14 17:12:15 +0700

    change jwt lib create token

--- api/Controller/userLogin.php
+++ api/Controller/userLogin.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
+require_once('../objects/userToken.php');
 require_once('../objects/user.php');
 require_once('../config/database.php');
 
@@ -12,7 +13,6 @@ $database = new ConfigAPI();
 $db = $database->getConnection();
 
 $user = new User($db);
-
 $data = json_decode(file_get_contents("php://input"));
 $user->user_name = $data->user_name;
 $user->user_password = $data->user_password;
@@ -20,12 +20,11 @@ $user->user_password = $data->user_password;
 $stmt = $user->login();
 
 if($stmt === true){
+    setcookie('token', $user->token, mktime()+60*60*5, "/", "greengarden.com", false, false);
     http_response_code(200);
-    echo json_encode(array(
-        "message" => "login success",
-        "code" => 200
-    ));
+    echo json_encode(array('data'=>$user->token, 'code'=>200));
 }
 else{
     echo json_encode($stmt);
 }
+?>
