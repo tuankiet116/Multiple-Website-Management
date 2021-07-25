4ab3b5c1df7c593cb5a2c7c21a18ddb40af740af
tuankiet116 <kietdat1612000@gmail.com>
2021-07-25 21:04:11 +0700

    api order for user

---
+++ api/Controller/UserOrder/orderCOD.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+include_once('../../objects/userToken.php');
+require_once('../../config/database.php');
+require_once('../../objects/orderUser.php');
+
+$database = new ConfigAPI();
+$db = $database->getConnection();
+
+$order = new OrderUser($db);
+$data = json_decode(file_get_contents("php://input"));
+$origin = $_SERVER['HTTP_ORIGIN'];
+if ($order->setWebID($origin) === true) {
+    $order->user_token = $data->user_token;
+    $order->order_payment = 1;
+    $result = $order->createOrder();
+    http_response_code(200);
+    echo json_encode($result);
+} else {
+    $message = array('code' => 500, 'message' => "This origin does not allow. If you're trying do something bad STOP now. We know about you. :)");
+    http_response_code(500);
+    echo json_encode($message);
+}
+?>
