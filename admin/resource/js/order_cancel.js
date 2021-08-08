256ec48719864e958c084d5e3d4a4fbf0dec9f09
tuankiet116 <kietdat1612000@gmail.com>
2021-08-08 08:14:23 +0700

    update request from FE

--- admin/resource/js/order_cancel.js
+++ admin/resource/js/order_cancel.js
@@ -41,7 +41,7 @@ function getOrder(term){
                         order_reason = "Không Xác Nhận Được Với Khách Hàng";
                     } 
                     else if(item.order_reason == 3){
-                        order_reason = "Khách Hàng Hủy Đơn Hoặc Trả Lại Hàng";
+                        order_reason = "Khách Hàng Hủy Đơn";
                     }
 
                     if(item.order_suspicious == 1 && item.order_payment == 2){
@@ -152,8 +152,8 @@ function valueDetail(data){
     else if(data.result.order_reason == 2){
         order_reason = 'Không Xác Nhận Được Với Khách Hàng'
     }
-    else if(data.result.order_reason == 3){
-        order_reason = 'Khách Hàng Hủy Đơn Hoặc Trả Lại Hàng'
+    else if(data.result.order_reason == 4){
+        order_reason = 'Khách Hàng Hủy Đơn'
     }
 
     if(data.result.order_suspicious == 1 && data.result.order_payment == 2){
